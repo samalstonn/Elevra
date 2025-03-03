ceabc6b3e9ee168b344d8cf6af7cd3dcc09be802
Sam Alston <sza6@cornell.edu>
2025-03-03 12:30:04 -0500

    add publishable key

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body className={inter.className}>
           <div className="flex flex-col min-h-screen">
