7f62929a1b35ca4a0ffdef4a463c38805447168c
Sam Alston <sza6@cornell.edu>
2025-03-29 11:10:53 -0400

    remove edit profile if not signed in

--- app/candidate/[name]/page.tsx
+++ app/candidate/[name]/page.tsx
@@ -1,29 +1,31 @@
 export const dynamic = "force-dynamic";
 import { notFound, redirect } from "next/navigation";
-import prisma from "@/prisma/prisma"; 
+import prisma from "@/prisma/prisma";
 import CandidateClient from "./CandidateClient";
-import { currentUser } from '@clerk/nextjs/server'
-
-
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function CandidatePage({
   searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const resolvedSearchParams = await searchParams;
   const candidateIDParam = resolvedSearchParams.candidateID;
   const electionIDParam = resolvedSearchParams.electionID;
-  
+
   if (!candidateIDParam || !electionIDParam) {
     // Redirect the user to the homepage if parameters are missing.
     redirect("/");
   }
-  
+
   // Remove any extra query parameters from the candidateID and electionID values
-  const candidateID = Array.isArray(candidateIDParam) ? candidateIDParam[0].split('?')[0] : candidateIDParam.split('?')[0];
-  const electionID = Array.isArray(electionIDParam) ? electionIDParam[0].split('?')[0] : electionIDParam.split('?')[0];
-  
+  const candidateID = Array.isArray(candidateIDParam)
+    ? candidateIDParam[0].split("?")[0]
+    : candidateIDParam.split("?")[0];
+  const electionID = Array.isArray(electionIDParam)
+    ? electionIDParam[0].split("?")[0]
+    : electionIDParam.split("?")[0];
+
   // Proceed with the query after ensuring the parameters are present:
   const election = await prisma.election.findUnique({
     where: { id: Number(electionID) },
@@ -32,7 +34,7 @@ export default async function CandidatePage({
   if (!election) {
     notFound();
   }
-  
+
   const candidate = await prisma.candidate.findFirst({
     where: {
       id: Number(candidateID),
@@ -49,12 +51,20 @@ export default async function CandidatePage({
     },
     include: {
       election: true,
-    }
+    },
   });
 
   const user = await currentUser();
   const currentUserId = user ? user.id : null;
-  const isEditable = currentUserId === candidate.clerkUserId
-  
-  return <CandidateClient candidate={candidate} election={election} suggestedCandidates={suggestedCandidates} isEditable={isEditable} />;
+  const isEditable =
+    currentUserId !== null && currentUserId === candidate.clerkUserId;
+
+  return (
+    <CandidateClient
+      candidate={candidate}
+      election={election}
+      suggestedCandidates={suggestedCandidates}
+      isEditable={isEditable}
+    />
+  );
 }
