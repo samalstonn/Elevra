634b9fe0952c8d7b796969d0bef694f93f0567ac
Sam Alston <sza6@cornell.edu>
2025-04-15 15:51:04 -0400

    add dynamic render

--- app/(admin)/admin/user-validation/page.tsx
+++ app/(admin)/admin/user-validation/page.tsx
@@ -1,6 +1,8 @@
 import prisma from "@/prisma/prisma";
 import AdminUserValidationClient from "./AdminUserValidationClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminUserValidationPage() {
   const validationRequests = await prisma.userValidationRequest.findMany({
     orderBy: { createdAt: "desc" },
